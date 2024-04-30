01a5dc08c65060bb957c405354627aed5fb89945
neel dave <neeldeveloper97@gmail.com>
2024-04-30 13:11:00 +0530

    Updated

--- src/main.ts
+++ src/main.ts
@@ -9,66 +9,60 @@ async function bootstrap() {
   console.log('Microservice is listening');
 }
 
-connect(
-  'amqp://cguser:YiZpmHXKQ8z3Rn93vFhx@165.22.92.187',
-  (err, connection) => {
+connect(process.env.rabbitMQServerAddress, (err, connection) => {
+  if (err) {
+    throw err;
+  }
+
+  connection.createChannel((err, channel) => {
     if (err) {
       throw err;
     }
 
-    connection.createChannel((err, channel) => {
-      if (err) {
-        throw err;
-      }
+    let paymentQueueName = 'payment_queue';
+    let paymentStatusQueueName = 'payment_status_queue';
 
-      let paymentQueueName = 'payment_queue';
-      let paymentStatusQueueName = 'payment_status_queue';
-
-      channel.assertQueue(paymentQueueName, {
-        durable: false,
-      });
-
-      console.log(
-        'Waiting for payment requests in %s. To exit, press CTRL+C',
-        paymentQueueName,
-      );
-
-      channel.consume(paymentQueueName, async (msg) => {
-        const walletAddress = msg.content.toString();
-        console.log(
-          'Received payment request for wallet address:',
-          walletAddress,
-        );
-
-        try {
-          const paymentService = new PaymentService();
-          const balance = await paymentService.processPayment(walletAddress);
-
-          console.log(
-            'Payment processed successfully. Wallet balance:',
-            balance,
-          );
-
-          channel.assertQueue(paymentStatusQueueName, {
-            durable: false,
-          });
-          channel.sendToQueue(
-            paymentStatusQueueName,
-            Buffer.from('Payment processed successfully'),
-          );
-        } catch (error) {
-          console.error('Error processing payment:', error.message);
-          channel.assertQueue(paymentStatusQueueName, {
-            durable: false,
-          });
-          channel.sendToQueue(
-            paymentStatusQueueName,
-            Buffer.from(`Error processing payment: ${error.message}`),
-          );
-        }
-      });
+    channel.assertQueue(paymentQueueName, {
+      durable: false,
     });
 
-    bootstrap();
-  },
-);
+    console.log(
+      'Waiting for payment requests in %s. To exit, press CTRL+C',
+      paymentQueueName,
+    );
+
+    channel.consume(paymentQueueName, async (msg) => {
+      const walletAddress = msg.content.toString();
+      console.log(
+        'Received payment request for wallet address:',
+        walletAddress,
+      );
+
+      try {
+        const paymentService = new PaymentService();
+        const balance = await paymentService.processPayment(walletAddress);
+
+        console.log('Payment processed successfully. Wallet balance:', balance);
+
+        channel.assertQueue(paymentStatusQueueName, {
+          durable: false,
+        });
+        channel.sendToQueue(
+          paymentStatusQueueName,
+          Buffer.from('Payment processed successfully'),
+        );
+      } catch (error) {
+        console.error('Error processing payment:', error.message);
+        channel.assertQueue(paymentStatusQueueName, {
+          durable: false,
+        });
+        channel.sendToQueue(
+          paymentStatusQueueName,
+          Buffer.from(`Error processing payment: ${error.message}`),
+        );
+      }
+    });
+  });
+
+  bootstrap();
+});
